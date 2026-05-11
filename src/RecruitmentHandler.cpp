4e52c399a687cd1ed5bb4a50f7de46c95d715fa5
Skyrim Modder <kulpuarif@gmail.com>
2026-05-11 21:26:00 +0300

    Emergency Debug v4.4: Testing IsPlayerTeammate

--- src/RecruitmentHandler.cpp
+++ src/RecruitmentHandler.cpp
@@ -24,19 +24,15 @@ namespace RecruitmentHandler
                 if (topicMgr && topicMgr->speaker) {
                     auto ref = topicMgr->speaker.get();
                     auto* speaker = ref ? ref->As<RE::Actor>() : nullptr;
-                    auto* player = RE::PlayerCharacter::GetSingleton();
                     
-                    if (speaker && player) {
-                        // ALTINI MANUEL SAYMA (0x0000000F)
-                        auto* goldObj = RE::TESForm::LookupByID<RE::TESBoundObject>(0x0000000F);
-                        int32_t goldCount = 0;
-                        if (goldObj) {
-                            goldCount = player->GetItemCount(goldObj);
-                        }
-                        
+                    if (speaker) {
+                        // TAKİPÇİ DURUMU TESTİ
+                        bool isFollower = speaker->IsPlayerTeammate();
                         std::string name = speaker->GetName();
+                        
                         char buf[256];
-                        snprintf(buf, sizeof(buf), "[NFS v4.3] %s - Manuel Altin: %d", name.c_str(), goldCount);
+                        snprintf(buf, sizeof(buf), "[NFS v4.4] %s - Takipci mi: %s", 
+                            name.c_str(), isFollower ? "EVET" : "HAYIR");
                         RE::DebugNotification(buf);
                     }
                 }
